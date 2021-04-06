46ee091ed952b22bdf4e9e12b5713a5247513ffd
jguzman55 <>
2021-04-06 20:39:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print('Welcome to the band name generator.\n')
+
+city = input('Which city did you grow up in?\n')
+
+print(city)
+
+food = input('What is your favorite food?\n')
+
+print(food)
+
+print('Your band name is ' + city + " " + food)
